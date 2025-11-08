da0b23933c6a1a48783a75524e41e3e9c3969ece
avi350751 <avishek350751@gmail.com>
2025-11-08 16:00:21 -0600

    Extract multiple webelements tcs added

---
+++ tests/test1.spec.js
@@ -0,0 +1,46 @@
+const {test, expect} = require('@playwright/test');
+
+test('test1 - Locators which extract multiple webelements', async ({page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    await page.locator('#username').fill('rahulshettyacademy');
+    await page.locator('#password').fill('learning');
+    await page.locator('#signInBtn').click();
+    const msg = await page.locator("//div[@class='col-lg-3']/h1").textContent();
+    console.log(msg);
+    expect(msg).toContain("Shop Name");
+
+    const cardTitles = await page.locator(".card-body a");
+    const txt = await cardTitles.first().textContent();
+    console.log(txt);
+    expect(txt).toContain("iphone X");
+
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+    expect(allTitles.length).toBe(4);
+
+
+})
+
+test('test2 - Locators which extract multiple webelements', async ({page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    await page.locator('#userEmail').fill('admin123@email.com');
+    await page.locator('#userPassword').fill('Weare@Oct2025#');
+    await page.locator('#login').click();
+    const msg = await page.locator(".mt-1 h3").textContent();
+    console.log(msg);
+    expect(msg).toContain("Automation");
+
+    const cardTitles = await page.locator(".card-body b");
+    const txt = await cardTitles.first().textContent();
+    console.log(txt);
+    expect(txt).toContain("ZARA COAT 3");
+
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+    expect(allTitles.length).toBe(3);
+
+
+})
